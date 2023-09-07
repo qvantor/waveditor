42733355f8247787b2fa1e0c597c9581a8169eff
sergeynikolaev <iintersergey@gmail.com>
2023-09-07 12:19:44 +0300

    forbes demo template added

--- apps/backend/src/app/services/demo-materials/templates/index.ts
+++ apps/backend/src/app/services/demo-materials/templates/index.ts
@@ -1,7 +1,9 @@
 import { BinanceTemplate } from './binance';
+import { ForbesTemplate } from './forbes';
 import { StackOverflowTemplate } from './stack-overflow';
 
 export const Templates = [
   { name: 'Demo Binance email template', version: BinanceTemplate },
   { name: 'Demo StackOverflow email template', version: StackOverflowTemplate },
+  { name: 'Demo Forbes email template', version: ForbesTemplate },
 ];
