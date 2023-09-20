ad14b26b9c46b30674840182e0cb5fa10970958d
sergeynikolaev <iintersergey@gmail.com>
2023-09-20 14:32:10 +0300

    removeElementById important fix

--- libs/editor-model/src/elements/elements/elements.actions.ts
+++ libs/editor-model/src/elements/elements/elements.actions.ts
@@ -1,25 +1,42 @@
 import { getConfigRootElementId } from '../../config';
 import { BuilderContext } from '../../types';
-import { getParentElement } from './';
+import { isLayoutStore } from '../layout';
+import { getElementById, getParentElement } from './';
 
-export const removeElementById =
-  ({
-    model: { elements, relations, config },
+const removeByIdInternal = (context: BuilderContext) => (id: string) => {
+  const {
+    model: { elements, relations },
+  } = context;
+  const element = getElementById(id)(elements.getValue());
+  const parent = getParentElement(elements.getValue(), id);
+  if (!parent || !element) return false;
+
+  if (isLayoutStore(element))
+    element
+      .getValue()
+      .params.columns.map((col) => col.children)
+      .flat()
+      .forEach(removeByIdInternal(context));
+
+  parent.actions.removeChild(id);
+  relations.actions.removeElementFontRelation(id);
+  elements.actions.removeElement(id);
+
+  return true;
+};
+export const removeElementById = (context: BuilderContext) => (id: string) => {
+  const {
+    model: { config },
     module: { undoRedo },
-  }: BuilderContext) =>
-  (id: string) => {
-    const rootElementId = getConfigRootElementId(config.getValue());
-    if (rootElementId === id) return false;
-    undoRedo.startBunch();
-    const parent = getParentElement(elements.getValue(), id);
-    if (parent) parent.actions.removeChild(id);
-    relations.actions.removeElementFontRelation(id);
-    elements.actions.removeElement(id);
-    undoRedo.endBunch();
-    return true;
-  };
+  } = context;
+  const rootElementId = getConfigRootElementId(config.getValue());
+  if (rootElementId === id) return false;
+  undoRedo.startBunch();
+  const removed = removeByIdInternal(context)(id);
+  undoRedo.endBunch();
+  return removed;
+};
 
-// @todo critical - remove layout children as well
 export const removeSelectedElement = (context: BuilderContext) => () => {
   const { selected } = context.interaction;
   const selectedValue = selected.getValue();
