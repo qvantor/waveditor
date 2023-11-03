e0ee4f443a1de9d7c1bfece628633b5982c22951
sergeynikolaev <iintersergey@gmail.com>
2023-11-03 12:45:55 +0300

    setMeta is ignored by undoRedo

--- libs/editor-model/src/services/effects.ts
+++ libs/editor-model/src/services/effects.ts
@@ -6,5 +6,6 @@ export const commonUndoRedoEffect = <V extends Element, A>({
   createUndoRedoEffect,
 }: UndoRedoModule<UndoRedoEvents>) =>
   createUndoRedoEffect<V, A, 'element'>('element', {
+    filterActions: ['setMeta'] as Array<keyof A>,
     filter: ({ payload }, value) => payload.next.id === value.id,
   });
